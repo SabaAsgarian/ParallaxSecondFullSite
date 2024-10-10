01f1ca1235426ff594476b4debcf023b746a6a30
Saba Asgarian <166124662+SabaAsgarian@users.noreply.github.com>
2024-10-10 18:23:13 +0330

    Update jar.jsx

--- src/app/components/jar.jsx
+++ src/app/components/jar.jsx
@@ -8,7 +8,7 @@ const Jar = ({ image, title }) => {
         <Card sx={{backgroundColor:'transparent' ,width: { xs: '100%', sm: '100%', md: '20%' }, margin: '5px',border:'0px' ,boxShadow:'none',height:'auto'}} className=" page2 updown">
             <CardContent>
                 <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'auto' }}>
-                    <Image src={image} alt={title} width={150} height={150} style={{transitionDuration: '1s', objectFit: 'cover',cursor:'pointer' }} className='img-hover'/>
+                    <Image src={image} alt={title}  style={{height:'70%',width:'40%',transitionDuration: '1s', objectFit: 'cover',cursor:'pointer' }} className='img-hover'/>
                 </Box>
                 <Typography variant="h3" sx={{ textAlign: 'center', marginTop: '10px' ,fontFamily:'gm',fontSize:'25px',fontWeight:'bolder',cursor:'pointer'}} className='p-hover font-extrabold '>
                     {title}
